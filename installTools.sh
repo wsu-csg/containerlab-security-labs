0839100eb11803021c2d1ce476ec2e8abb554f1f
javds <javds1@outlook.com>
2025-11-20 17:32:20 -0800

    fix install script

--- installTools.sh
+++ installTools.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sudo apt install docker.io -y
+sudo apt install containerd.io -y
+sudo apt install remmina -y
 sudo apt install openvswitch -y
 sudo bash -c "$(curl -sL https://get.containerlab.dev)"
